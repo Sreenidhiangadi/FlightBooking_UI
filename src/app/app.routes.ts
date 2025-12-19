e7a4813b2ec20261b68dc9b5228199244f96b52a
Sreenidhi Angadi <angadisreenidhi@gmail.com>
2025-12-19 09:20:16 +0530

    updated app component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,28 @@ import { FlightSearchComponent } from './flights/flight-search/flight-search';
 import { LoginComponent } from './auth/login/login';
 import { RegisterComponent } from './auth/register/register';
 import { HomeComponent } from './home/home';
+import { authGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
-  {  path: '', component: HomeComponent },
+  { path: '', component: HomeComponent },
   { path: 'flights', component: FlightSearchComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: '**', redirectTo: 'flights' }
+  {
+  path: 'all-flights',
+  loadComponent: () =>
+    import('./all-flights/all-flights')
+      .then(m => m.AllFlightsComponent)
+},
+
+  {path: 'booking/:flightId',
+    loadComponent: () =>
+      import('./booking/booking').then(m => m.Booking),
+    canActivate: [authGuard]
+  },
+
+  { path: '**', redirectTo: 'flights' } 
 ];
+
 
 export const routerProviders = provideRouter(routes);
