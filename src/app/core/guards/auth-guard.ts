1e3abed2d88c1dc2d991a0fbb273bf5c66b14e06
Sreenidhi Angadi <angadisreenidhi@gmail.com>
2025-12-19 09:25:04 +0530

    updated auth components

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -1,12 +1,14 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
-import { TokenService } from '../services/token.service';
+import { AuthService } from '../services/auth.service';
 
 export const authGuard: CanActivateFn = () => {
-  const token = inject(TokenService);
+  const authService = inject(AuthService);
   const router = inject(Router);
 
-  if (token.isLoggedIn()) return true;
+  if (authService.isLoggedIn()) {
+    return true;
+  }
 
   router.navigate(['/login']);
   return false;
--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -3,7 +3,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('token');
 
-  // Attach token ONLY if it exists
+  
   if (token) {
     req = req.clone({
       setHeaders: { Authorization: `Bearer ${token}` }
